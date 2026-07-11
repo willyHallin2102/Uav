3b17802c3c7173df2367507bf4c5d4bc32f7ab23
willy <wiha2102@student.miun.se>
2026-07-11 16:53:27 +0200

    mm, test_script.py runs from root, yield: Tensorflow Version: 2.20.0 Sionna Version: 2.0.1 Numpy Version: 2.4.6 ----------------- Numpy should not been exceeded version 2.0

---
+++ test_script.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import sionna as sn
+import numpy as np
+
+
+print(f"Tensorflow Version: {tf.__version__}")
+print(f"Sionna Version: {sn.__version__}")
+print(f"Numpy Version: {np.__version__}")
