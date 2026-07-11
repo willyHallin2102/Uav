0e522cf2a2a38a3132f26b7d39c209c3c34b25fb
willy <wiha2102@student.miun.se>
2026-07-11 16:48:43 +0200

    updated the broken installation script, also made a simpler version of installation

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 
 # name of the virtual environment as well as define python version
 VENV=".venv"
-PYTHON="{1:-python3.11}"
+PYTHON="${1:-python3.11}"
 
 "$PYTHON" -m venv "$VENV"
 
@@ -27,26 +27,24 @@ python -m pip install --upgrade pip setuptools wheel
 # ----------------------------------------
 # 3rd party Libraries 
 
-cat > requirements.in << 'EOF'
-tensorflow[and-cuda]==2.20.0
-pandas
-numpy<2.0
-scipy
-pyarrow
-numba
-matplotlib
-seaborn
-tqdm
-orjson
-einops
-h5py
-plotly
-tensorboard
-git+https://github.com/NVlabs/sionna.git@main
-EOF
+pip install                         \
+    "tensorflow[and-cuda]==2.20.0"  \
+    pandas                          \
+    "numpy<2.0"                     \
+    scipy                           \
+    pyarrow                         \
+    numba                           \
+    matplotlib                      \
+    seaborn                         \
+    tqdm                            \
+    orjson                          \
+    einops                          \
+    h5py                            \
+    plotly                          \
+    tensorboard
 
-# Generate lock-file with hashes
-pip-compile --generate-hashes requirements.in
 
-# Install from the lockfile
-pip install -r requirements.txt
+pip install git+https://github.com/NVlabs/sionna.git@main
+
+pip freeze > requirements.txt
+
