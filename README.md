9ba9f68e89666ee7def98bf9429815874ffb3944
willy <wiha2102@student.miun.se>
2026-07-11 16:24:59 +0200

    First testing Upload, gitignore, the installation script to test run installation works

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Uav
 Generative Artificial UAV Path-Modeling
+
+```bash
+chmod +x install.sh
+./install.sh
+
+``` 
---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Setting up error management during installation.
+#
+#   - A command exits with a non-zero status ``-e``
+#   - An undefined variable is being used ``-u``
+#   - A pipeline fails anywhere ``-o pipefail``
+#
+set -euo pipefail
+
+# name of the virtual environment as well as define python version
+VENV=".venv"
+PYTHON="{1:-python3.11}"
+
+"$PYTHON" -m venv "$VENV"
+
+# Activates the environment
+#   -- Shellcheck disable=SC1090
+source "$VENV/bin/activate"
+
+# ----------------------------------------
+# Core Tooling
+
+python -m pip install --upgrade pip setuptools wheel
+
+
+# ----------------------------------------
+# 3rd party Libraries 
+
+cat > requirements.in << 'EOF'
+tensorflow[and-cuda]==2.20.0
+pandas
+numpy<2.0
+scipy
+pyarrow
+numba
+matplotlib
+seaborn
+tqdm
+orjson
+einops
+h5py
+plotly
+tensorboard
+git+https://github.com/NVlabs/sionna.git@main
+EOF
+
+# Generate lock-file with hashes
+pip-compile --generate-hashes requirements.in
+
+# Install from the lockfile
+pip install -r requirements.txt
